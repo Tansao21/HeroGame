18cef8f5d24a9765dfa6f6d40c3a2f7fa991c822
Николай Мягков <tansao26@gmail.com>
2022-12-26 13:52:33 +0300

    Добавьте файлы проекта.

---
+++ HeroGame/Cell.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroGame
+{
+	public enum Cell
+	{
+		Empty = '*',
+		Wall = '#',
+		Portal = 'O',
+		Bound = 'x',
+		
+	}
+}
---
+++ HeroGame/Constants.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroGame
+{
+	public enum Constants
+	{
+		MinRows = 10,
+		MaxRows = 20,
+
+		MinCols = 10,
+		MaxCols = 30,
+
+		StartIHero = 1,
+		StartJHero = 1,
+
+		HeroSkin = 'H',
+
+		StartIDog = 5,
+		StartJDog = 5,
+
+		DogSkin = '@',
+
+		WallPercent = 5
+	}
+}
